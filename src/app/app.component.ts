575a5b5178f8454ca29ce7ac315df34e5cdd4483
BOUTOUILSafaa <safae.boutouil@gmail.com>
2018-07-23 21:49:29 +0100

    added my first parameters + modified the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  contact = {
+      name: 'safaa',
+      email: 'safae.boutouil@gmail.com'
+  };
 }
