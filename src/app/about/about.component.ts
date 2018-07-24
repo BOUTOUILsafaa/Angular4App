4ad206e49324ca183d61d33ffa502f180bc14fa8
BOUTOUILSafaa <safae.boutouil@gmail.com>
2018-07-24 01:42:29 +0100

    added data binding in one direction ts => html + added pipe in date

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  info = {
+    nom: 'safaa',
+    email: 'safae.boutouil@gmail.com',
+  }
+  comments = [
+      { date: new Date(), message: 'A'},
+      { date: new Date(), message: 'B'},
+      { date: new Date(), message: 'C'},
+  ];
   constructor() { }
 
   ngOnInit() {
