e06b63242bd1bd21b294b2a10478a3ef54f934ba
BOUTOUILSafaa <safae.boutouil@gmail.com>
2018-07-31 01:14:16 +0100

    added pagination

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -9,18 +9,31 @@ import {map} from 'rxjs/internal/operators';
 })
 export class GalleryComponent implements OnInit {
   pagePhotos: {};
+  motCle: String = '';
+  currentPage = 1;
+  size = 4;
+  totalPages: number;
+  pages: Array<number> = [];
   constructor( private http: HttpClient) { }
 
   ngOnInit() {
   }
   onSearch(dataForm) {
     console.log(dataForm);
-    this.http.get('https://pixabay.com/api/?key=9682692-25f8b75ca0f48c427241f45af&q=' + dataForm.motCle + '&per_page=10&page=1')
+    this.http.get('https://pixabay.com/api/?key=9682692-25f8b75ca0f48c427241f45af&q=' + dataForm.motCle + '&per_page=' + this.size + '&page=' + this.currentPage)
         .pipe(map((response: any) => response))
-        .subscribe(data => {
-            console.log(data);
+        .subscribe(data => {console.log(data);
           this.pagePhotos = data;
+          this.totalPages = data.totalHits / this.size;
+          if (data.totalHits % this.size !=0) { ++this.totalPages; }
+          this.pages = new Array(this.totalPages);
         });
   }
+  goToPage(i) {
+      this.currentPage = i;
+      this.onSearch({motCle: this.motCle});
+
+  }
+
 
 }
